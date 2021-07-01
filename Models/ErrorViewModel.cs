54b2dc920ab29d281d987421a3c1239d070cd2ae
dotnet44 <dotnet_44@hotmail.com>
2021-07-01 12:33:06 +0530

    all commit file test-mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
